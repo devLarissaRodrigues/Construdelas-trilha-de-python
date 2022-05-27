3769533fbd35f63a71768c521a974cf68c2843d2
devLarissaRodrigues <101843983+devLarissaRodrigues@users.noreply.github.com>
2022-05-27 16:41:45 -0300

    Delete desafio_11.py

--- desafio_11.py
+++
@@ -1,8 +0,0 @@
-# Dado um arquivo de entrada, escreva um programa que leia o conteudo do arquivo para uma string,
-# e escreva um outro arquivo de saída que imprima o texto ao contrário.
-# Exemplo de entrada: Oi mulheres maravilhosas do curso de Python do ConstruDelas!
-# Exemplo de saída: !saleDurtsnoC od nohtyP ed\nosruc od sasohlivaram serehlum iO
-
-frase = input("Digite uma frase que deseje inverter: ")
-
-print(frase[::-1])
